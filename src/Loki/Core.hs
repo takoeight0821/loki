917f06f7837e8d9a181bfd18d3d71176435a2a25
Yuya Kono <takohati0821@gmail.com>
2024-11-08 08:56:38 +0000

    Refactor Loki.Core and Loki.Eval to include location tracking in Producers, Consumers, and Statements; enhance error handling with call stack information

--- src/Loki/Core.hs
+++ src/Loki/Core.hs
@@ -10,47 +10,72 @@ module Loki.Core
     Name (..),
     Unique (..),
     newName,
+    Location (..),
+    HasLocation (..),
+    locCS,
+    LocationRepr (..),
     ex1,
+    ex2,
   )
 where
 
 import Data.Text (Text)
+import Data.Traversable (for)
 import Effectful
 import Effectful.State.Static.Shared (State, evalState, get, put)
+import GHC.Exception (CallStack, SrcLoc (..), getCallStack)
+import GHC.Stack (HasCallStack, callStack)
 import Loki.Prelude
 
 data Producer a where
-  Var :: a -> Producer a
-  Const :: Const -> Producer a
-  Do :: a -> Statement a -> Producer a
+  Var :: Location -> a -> Producer a
+  Const :: Location -> Const -> Producer a
+  Do :: Location -> a -> Statement a -> Producer a
 
 deriving stock instance (Show a) => Show (Producer a)
 
+instance HasLocation (Producer a) where
+  location (Var loc _) = loc
+  location (Const loc _) = loc
+  location (Do loc _ _) = loc
+
 data Consumer a where
-  Finish :: Consumer a
-  Label :: a -> Consumer a
+  Finish :: Location -> Consumer a
+  Label :: Location -> a -> Consumer a
 
 deriving stock instance (Show a) => Show (Consumer a)
 
+instance HasLocation (Consumer a) where
+  location (Finish loc) = loc
+  location (Label loc _) = loc
+
 data Statement a where
-  Prim :: Text -> [Producer a] -> Consumer a -> Statement a
-  Switch :: Producer a -> [(Const, Statement a)] -> Statement a -> Statement a
-  Cut :: Producer a -> Consumer a -> Statement a
+  Prim :: Location -> Text -> [Producer a] -> Consumer a -> Statement a
+  Switch :: Location -> Producer a -> [(Const, Statement a)] -> Statement a -> Statement a
+  Cut :: Location -> Producer a -> Consumer a -> Statement a
 
 deriving stock instance (Show a) => Show (Statement a)
+
+instance HasLocation (Statement a) where
+  location (Prim loc _ _ _) = loc
+  location (Switch loc _ _ _) = loc
+  location (Cut loc _ _) = loc
 
 prim :: (State Unique :> es) => Text -> [Producer Name] -> Eff es (Producer Name)
 prim name args = do
   label <- newName name
-  return $ Do label (Prim name args (Label label))
+  return $ Do locCS label (Prim locCS name args (Label locCS label))
 
-switch :: (State Unique :> es) => Producer Name -> [(Const, Producer Name)] -> Producer Name -> Eff es (Producer Name)
+switch :: (State Unique :> es) => Producer Name -> [(Const, Eff es (Producer Name))] -> Producer Name -> Eff es (Producer Name)
 switch expr clauses def = do
   label <- newName "switch"
+  clauses' <- for clauses \(c, p) -> do
+    p' <- p
+    return (c, Cut locCS p' (Label locCS label))
   return
-    $ Do label
-    $ Switch expr (map (\(c, p) -> (c, Cut p (Label label))) clauses)
-    $ Cut def (Label label)
+    $ Do locCS label
+    $ Switch locCS expr clauses'
+    $ Cut locCS def (Label locCS label)
 
 data Const where
   Int :: Integer -> Const
@@ -79,4 +104,44 @@ ex1 :: Producer Name
 ex1 =
   runPureEff
     $ evalState (Unique 0)
-    $ prim "add" [Const (Int 1), Const (Int 2)]
+    $ prim "add" [Const locCS (Int 1), Const locCS (Int 2)]
+
+ex2 :: Producer Name
+ex2 =
+  runPureEff
+    $ evalState (Unique 0) do
+      switch
+        (Const locCS (Int 1))
+        [(Int 1, prim "print" [Const locCS (Int 1)])]
+        (Const locCS (Int 3))
+
+data Location = forall a. (LocationRepr a) => Location a
+
+instance Show Location where
+  show (Location loc) = locationFile loc <> ":" <> show (locationLine loc) <> ":" <> show (locationColumn loc)
+
+class HasLocation a where
+  location :: a -> Location
+
+class LocationRepr a where
+  locationFile :: a -> String
+  locationLine :: a -> Int
+  locationColumn :: a -> Int
+
+instance LocationRepr CallStack where
+  locationFile cs =
+    case getCallStack cs of
+      [] -> "Unknown location"
+      ((_, SrcLoc {srcLocFile}) : _) -> srcLocFile
+  locationLine cs =
+    case getCallStack cs of
+      [] -> 0
+      ((_, SrcLoc {srcLocStartLine}) : _) -> srcLocStartLine
+  locationColumn cs =
+    case getCallStack cs of
+      [] -> 0
+      ((_, SrcLoc {srcLocStartCol}) : _) -> srcLocStartCol
+
+-- | locCS is a helper function that creates a 'Location' from the current call stack.
+locCS :: (HasCallStack) => Location
+locCS = Location callStack
--- src/Loki/Eval.hs
+++ src/Loki/Eval.hs
@@ -8,16 +8,33 @@ where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Map qualified as M
+import Data.Text (Text)
 import Effectful (Eff, IOE, runEff, (:>))
+import Effectful.Error.Static (CallStack, Error, HasCallStack, prettyCallStack, runErrorWith, throwError)
+import Effectful.FileSystem.IO (stderr)
 import Effectful.Reader.Static (Reader, ask, local, runReader)
 import Loki.Core
 import Loki.Prelude
+import System.IO (hPutStrLn)
 
-eval :: Env -> Statement Name -> IO ()
-eval env stmt = runEff $ runReader env $ evalStatement stmt
+eval :: (HasCallStack) => Env -> Statement Name -> IO ()
+eval env stmt = runEff $ runReader env $ runErrorWith handler $ evalStatement stmt
+  where
+    handler :: (IOE :> es) => CallStack -> EvalError -> Eff es ()
+    handler stack err = liftIO $ do
+      hPutStrLn stderr $ "Error: " <> show err
+      hPutStrLn stderr $ prettyCallStack stack
 
-evalStatement :: (Reader Env :> es, IOE :> es) => Statement Name -> Eff es ()
-evalStatement (Prim name args cont) = do
+data EvalError
+  = InvalidArguments Location [Value]
+  | UnknownPrimitive Location Text
+  | UndefinedVariable Location Name
+  | UndefinedLabel Location Name
+  | UnexpectedProducer Location (Producer Name)
+  deriving stock (Show)
+
+evalStatement :: (HasCallStack, Reader Env :> es, IOE :> es, Error EvalError :> es) => Statement Name -> Eff es ()
+evalStatement (Prim loc name args cont) = do
   args' <- traverse evalProducer args
   cont' <- evalConsumer cont
 
@@ -25,42 +42,34 @@ evalStatement (Prim name args cont) = do
     "add" -> case args' of
       [VInt x, VInt y] -> do
         apply cont' (VInt (x + y))
-      _ -> error "invalid arguments"
-    _ -> error "unknown primitive"
-evalStatement (Switch prod clauses def) = do
+      _ -> throwError $ InvalidArguments loc args'
+    _ -> throwError $ UnknownPrimitive loc name
+evalStatement (Switch _ prod clauses def) = do
   value <- evalProducer prod
   go value clauses
   where
     go _ [] = evalStatement def
     go (VInt m) ((Int n, stmt) : _) | m == n = evalStatement stmt
     go value (_ : rest) = go value rest
-evalStatement (Cut (Do label stmt) cont) = do
+evalStatement (Cut _ (Do _ label stmt) cont) = do
   covalue <- evalConsumer cont
   local (insertCoValue label covalue) do
     evalStatement stmt
-evalStatement (Cut prod cont) = do
+evalStatement (Cut _ prod cont) = do
   value <- evalProducer prod
   covalue <- evalConsumer cont
   apply covalue value
 
-evalProducer :: (Reader Env :> es) => Producer Name -> Eff es Value
-evalProducer (Var name) = do
-  env <- ask
-  case lookupValue name env of
-    Just value -> return value
-    Nothing -> error "undefined variable"
-evalProducer (Const (Int n)) = return (VInt n)
-evalProducer (Do _ _) = error "unexpected statement"
+evalProducer :: (HasCallStack, Reader Env :> es, Error EvalError :> es) => Producer Name -> Eff es Value
+evalProducer (Var loc name) = lookupValue loc name
+evalProducer (Const _ (Int n)) = return (VInt n)
+evalProducer p = throwError $ UnexpectedProducer (location p) p
 
-evalConsumer :: (Reader Env :> es) => Consumer Name -> Eff es CoValue
-evalConsumer Finish = return CFinish
-evalConsumer (Label name) = do
-  env <- ask
-  case lookupCoValue name env of
-    Just value -> return value
-    Nothing -> error "undefined label"
+evalConsumer :: (HasCallStack, Reader Env :> es, Error EvalError :> es) => Consumer Name -> Eff es CoValue
+evalConsumer (Finish _) = return CFinish
+evalConsumer (Label loc name) = lookupCoValue loc name
 
-apply :: (IOE :> es) => CoValue -> Value -> Eff es ()
+apply :: (HasCallStack, IOE :> es) => CoValue -> Value -> Eff es ()
 apply CFinish x = liftIO $ print x
 
 data Env = Env
@@ -75,11 +84,19 @@ instance Semigroup Env where
 instance Monoid Env where
   mempty = Env mempty mempty
 
-lookupValue :: Name -> Env -> Maybe Value
-lookupValue name = M.lookup name . (.values)
+lookupValue :: (HasCallStack, Reader Env :> es, Error EvalError :> es) => Location -> Name -> Eff es Value
+lookupValue loc name = do
+  env <- ask @Env
+  case M.lookup name env.values of
+    Just value -> return value
+    Nothing -> throwError $ UndefinedVariable loc name
 
-lookupCoValue :: Name -> Env -> Maybe CoValue
-lookupCoValue name = M.lookup name . (.covalues)
+lookupCoValue :: (HasCallStack, Reader Env :> es, Error EvalError :> es) => Location -> Name -> Eff es CoValue
+lookupCoValue loc name = do
+  env <- ask @Env
+  case M.lookup name env.covalues of
+    Just value -> return value
+    Nothing -> throwError $ UndefinedLabel loc name
 
 insertCoValue :: Name -> CoValue -> Env -> Env
 insertCoValue name value env = env {covalues = M.insert name value env.covalues}
